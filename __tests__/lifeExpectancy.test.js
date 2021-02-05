8b9d903df9e98d782136e6fd83631e7751a4ce45
Andy Lawson <alawson89@gmail.com>
2021-02-05 12:07:44 -0800

    add good fail for 'earthExpectancy' test

--- __tests__/lifeExpectancy.test.js
+++ __tests__/lifeExpectancy.test.js
@@ -1,14 +1,16 @@
 import LifeExpectancy from "../src/js/lifeExpectancy.js";
 
 describe("LifeExpectancy", () => {
-  test("Should return a planet, age, life expectancy, and years left to live on said planet.", () => {
-    const newLifeExpectancy = new LifeExpectancy("Earth", 50, 73, 23);
+  test("Should return a planet, age, and life expectancy on said planet.", () => {
+    const newLifeExpectancy = new LifeExpectancy("Earth", 50, 73);
     expect(newLifeExpectancy.planet).toEqual("Earth");
     expect(newLifeExpectancy.age).toEqual(50);
     expect(newLifeExpectancy.expectancy).toEqual(73);
-    expect(newLifeExpectancy.yearsLeft).toEqual(23);
   });
 
-  
+  test("Should return average Earth life expectancy, user's age in Earth years, and how many Earth years they have left live.", () => {
+    const newLifeExpectancy = new LifeExpectancy("Earth", 31, 73);
+    expect(newLifeExpectancy.earthExpectancy()).toEqual("Average life expectancy for your demographic on Earth: 73. Your age in Earth years: 31. Earth years left to live: 41");
+  });
 
 });
--- src/js/lifeExpectancy.js
+++ src/js/lifeExpectancy.js
@@ -1,8 +1,12 @@
 export default class LifeExpectancy {
-  constructor (planet, age, expectancy, yearsLeft) {
+  constructor (planet, age, expectancy) {
     this.planet = planet;
     this.age = age;
     this.expectancy = expectancy;
-    this.yearsLeft = yearsLeft;
   }
+
+  earthExpectancy () {
+    return `Average life expectancy for your demographic on Earth: ${this.expectancy}. Your age in ${this.planet} years: ${this.age}. ${this.planet} years left to live: ${Math.round(this.expectancy - this.age)}`;
+  }
+
 }
