76ee1fd7c6a4d47230ccaedb813abdef19c2c9c7
Andy Lawson <alawson89@gmail.com>
2021-02-05 12:08:55 -0800

    add successful 'earthExpectancy' test

--- __tests__/lifeExpectancy.test.js
+++ __tests__/lifeExpectancy.test.js
@@ -10,7 +10,7 @@ describe("LifeExpectancy", () => {
 
   test("Should return average Earth life expectancy, user's age in Earth years, and how many Earth years they have left live.", () => {
     const newLifeExpectancy = new LifeExpectancy("Earth", 31, 73);
-    expect(newLifeExpectancy.earthExpectancy()).toEqual("Average life expectancy for your demographic on Earth: 73. Your age in Earth years: 31. Earth years left to live: 41");
+    expect(newLifeExpectancy.earthExpectancy()).toEqual("Average life expectancy for your demographic on Earth: 73. Your age in Earth years: 31. Earth years left to live: 42");
   });
 
 });
