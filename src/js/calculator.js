ad4787203d841e1ade50f45db21e927e54f15377
Andy Lawson <alawson89@gmail.com>
2021-02-05 14:39:59 -0800

    add good fail for if lived beyond life expectancy for 'earthExpectancy' test

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -3,23 +3,23 @@ export default class PlanetAgeCalculator {
     this.planet = planet;
     this.age = age;
   }
-
+  
   earthAge() {
     return `Age in ${this.planet} years = ${this.age}`;
   }
-
+  
   mercuryAge() {
     return `Age in ${this.planet} years = ${Math.round(this.age / .24)}`;
   }
-
+  
   venusAge() {
     return `Age in ${this.planet} years = ${Math.round(this.age / .62)}`;
   }
-
+  
   marsAge() {
     return `Age in ${this.planet} years = ${Math.round(this.age / 1.88)}`;
   }
-
+  
   jupiterAge() {
     return `Age in ${this.planet} years = ${Math.round(this.age / 11.86)}`;
   }
