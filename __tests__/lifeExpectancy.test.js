f355d3c3572f2f7f2e63e0e0ea3288326dcc803a
Andy Lawson <alawson89@gmail.com>
2021-02-05 14:08:12 -0800

    troubleshoot coverage for all lines when if/else statement is in use

--- __tests__/lifeExpectancy.test.js
+++ __tests__/lifeExpectancy.test.js
@@ -8,29 +8,34 @@ describe("LifeExpectancy", () => {
     expect(newLifeExpectancy.expectancy).toEqual(73);
   });
 
-  test("Should return average Earth life expectancy, user's age in Earth years, and how many Earth years they have left to live.", () => {
-    const newLifeExpectancy = new LifeExpectancy("Earth", 50, 73);
-    expect(newLifeExpectancy.earthExpectancy()).toEqual("Average life expectancy for your demographic on Earth: 73. Your age in Earth years: 50. Earth years left to live: 23");
+  // test("Should return average Earth life expectancy, user's age in Earth years, and how many Earth years they have left to live based on average life expectancy.", () => {
+  //   const newLifeExpectancy = new LifeExpectancy("Earth", 50, 73);
+  //   expect(newLifeExpectancy.earthExpectancy()).toEqual("Average life expectancy for your demographic on Earth: 73. Your age in Earth years: 50. Earth years left to live: 23");
+  // });
+
+  test("Should return average Earth life expectancy, user's age in Earth years, and how many years they have lived past average life expectancy.", () => {
+    const newLifeExpectancy = new LifeExpectancy("Earth", 75, 73);
+    expect(newLifeExpectancy.earthExpectancy()).toEqual("Average life expectancy for your demographic on Earth: 73. Your age in Earth years: 75. Years lived beyond life expectancy: 2.");
   });
 
-  test("Should return average Mercury life expectancy, user's age in Mercury years, and how many Mercury years they have left to live.", () => {
-    const newLifeExpectancy = new LifeExpectancy("Mercury", 50, 73);
-    expect(newLifeExpectancy.mercuryExpectancy()).toEqual("Average life expectancy for your demographic on Mercury: 304. Your age in Mercury years: 208. Mercury years left to live: 96");
-  });
+  // test("Should return average Mercury life expectancy, user's age in Mercury years, and how many Mercury years they have left to live.", () => {
+  //   const newLifeExpectancy = new LifeExpectancy("Mercury", 50, 73);
+  //   expect(newLifeExpectancy.mercuryExpectancy()).toEqual("Average life expectancy for your demographic on Mercury: 304. Your age in Mercury years: 208. Mercury years left to live: 96");
+  // });
 
-  test("Should return average Venus life expectancy, user's age in Venus years, and how many Venus years they have left to live.", () => {
-    const newLifeExpectancy = new LifeExpectancy("Venus", 50, 73);
-    expect(newLifeExpectancy.venusExpectancy()).toEqual("Average life expectancy for your demographic on Venus: 118. Your age in Venus years: 81. Venus years left to live: 37");
-  });
+  // test("Should return average Venus life expectancy, user's age in Venus years, and how many Venus years they have left to live.", () => {
+  //   const newLifeExpectancy = new LifeExpectancy("Venus", 50, 73);
+  //   expect(newLifeExpectancy.venusExpectancy()).toEqual("Average life expectancy for your demographic on Venus: 118. Your age in Venus years: 81. Venus years left to live: 37");
+  // });
 
-  test("Should return average Mars life expectancy, user's age in Mars years, and how many Mars years they have left to live.", () => {
-    const newLifeExpectancy = new LifeExpectancy("Mars", 50, 73);
-    expect(newLifeExpectancy.marsExpectancy()).toEqual("Average life expectancy for your demographic on Mars: 39. Your age in Mars years: 27. Mars years left to live: 12");
-  });
+  // test("Should return average Mars life expectancy, user's age in Mars years, and how many Mars years they have left to live.", () => {
+  //   const newLifeExpectancy = new LifeExpectancy("Mars", 50, 73);
+  //   expect(newLifeExpectancy.marsExpectancy()).toEqual("Average life expectancy for your demographic on Mars: 39. Your age in Mars years: 27. Mars years left to live: 12");
+  // });
 
-  test("Should return average Jupiter life expectancy, user's age in Jupiter years, and how many Jupiter years they have left to live.", () => {
-    const newLifeExpectancy = new LifeExpectancy("Jupiter", 50, 73);
-    expect(newLifeExpectancy.jupiterExpectancy()).toEqual("Average life expectancy for your demographic on Jupiter: 6. Your age in Jupiter years: 4. Jupiter years left to live: 2");
-  });
+  // test("Should return average Jupiter life expectancy, user's age in Jupiter years, and how many Jupiter years they have left to live.", () => {
+  //   const newLifeExpectancy = new LifeExpectancy("Jupiter", 50, 73);
+  //   expect(newLifeExpectancy.jupiterExpectancy()).toEqual("Average life expectancy for your demographic on Jupiter: 6. Your age in Jupiter years: 4. Jupiter years left to live: 2");
+  // });
 
 });
--- src/js/lifeExpectancy.js
+++ src/js/lifeExpectancy.js
@@ -1,28 +1,32 @@
 export default class LifeExpectancy {
-  constructor (planet, age, expectancy) {
+  constructor(planet, age, expectancy) {
     this.planet = planet;
     this.age = age;
     this.expectancy = expectancy;
   }
 
-  earthExpectancy () {
-    return `Average life expectancy for your demographic on ${this.planet}: ${this.expectancy}. Your age in ${this.planet} years: ${this.age}. ${this.planet} years left to live: ${Math.round(this.expectancy - this.age)}`;
+  earthExpectancy() {
+    if (this.age > this.expectancy) {
+      return `Average life expectancy for your demographic on ${this.planet}: ${this.expectancy}. Your age in ${this.planet} years: ${this.age}. Years lived beyond life expectancy: ${Math.round(this.age - this.expectancy)}.`;
+    } else {
+      return `Average life expectancy for your demographic on ${this.planet}: ${this.expectancy}. Your age in ${this.planet} years: ${this.age}. ${this.planet} years left to live: ${Math.round(this.expectancy - this.age)}`;
+    }
   }
 
-  mercuryExpectancy () {
-    return `Average life expectancy for your demographic on ${this.planet}: ${Math.round(this.expectancy / .24)}. Your age in ${this.planet} years: ${Math.round(this.age / .24)}. ${this.planet} years left to live: ${Math.round((this.expectancy - this.age) / .24)}`;
-  }
+  // mercuryExpectancy () {
+  //   return `Average life expectancy for your demographic on ${this.planet}: ${Math.round(this.expectancy / .24)}. Your age in ${this.planet} years: ${Math.round(this.age / .24)}. ${this.planet} years left to live: ${Math.round((this.expectancy - this.age) / .24)}`;
+  // }
 
-  venusExpectancy () {
-    return `Average life expectancy for your demographic on ${this.planet}: ${Math.round(this.expectancy / .62)}. Your age in ${this.planet} years: ${Math.round(this.age / .62)}. ${this.planet} years left to live: ${Math.round((this.expectancy - this.age) / .62)}`;
-  }
+  // venusExpectancy () {
+  //   return `Average life expectancy for your demographic on ${this.planet}: ${Math.round(this.expectancy / .62)}. Your age in ${this.planet} years: ${Math.round(this.age / .62)}. ${this.planet} years left to live: ${Math.round((this.expectancy - this.age) / .62)}`;
+  // }
 
-  marsExpectancy () {
-    return `Average life expectancy for your demographic on ${this.planet}: ${Math.round(this.expectancy / 1.88)}. Your age in ${this.planet} years: ${Math.round(this.age / 1.88)}. ${this.planet} years left to live: ${Math.round((this.expectancy - this.age) / 1.88)}`;
-  }
+  // marsExpectancy () {
+  //   return `Average life expectancy for your demographic on ${this.planet}: ${Math.round(this.expectancy / 1.88)}. Your age in ${this.planet} years: ${Math.round(this.age / 1.88)}. ${this.planet} years left to live: ${Math.round((this.expectancy - this.age) / 1.88)}`;
+  // }
 
-  jupiterExpectancy () {
-    return `Average life expectancy for your demographic on ${this.planet}: ${Math.round(this.expectancy / 11.86)}. Your age in ${this.planet} years: ${Math.round(this.age / 11.86)}. ${this.planet} years left to live: ${Math.round((this.expectancy - this.age) / 11.86)}`;
-  }
+  // jupiterExpectancy () {
+  //   return `Average life expectancy for your demographic on ${this.planet}: ${Math.round(this.expectancy / 11.86)}. Your age in ${this.planet} years: ${Math.round(this.age / 11.86)}. ${this.planet} years left to live: ${Math.round((this.expectancy - this.age) / 11.86)}`;
+  // }
 
 }
