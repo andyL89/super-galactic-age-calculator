3893a4d963fdd278b1bbd7df1500a1b79cd60b1c
Andy Lawson <alawson89@gmail.com>
2021-02-05 15:30:10 -0800

    add successful if lived beyond life expectancy 'jupiterExpectancy' test

--- src/js/lifeExpectancy.js
+++ src/js/lifeExpectancy.js
@@ -39,7 +39,7 @@ export default class LifeExpectancy {
 
   jupiterExpectancy() {
     if (Math.round((this.age / 11.86) > (this.expectancy / 11.86))) {
-      return `Average life expectancy for your demographic on ${this.planet}: ${Math.round(this.expectancy / 11.86)}. Your age in ${this.planet} years: ${Math.round(this.age / 11.86)}. Years lived beyond average life expectancy: ${Math.round((this.age - this.expectancy) / 11.86)}.`;
+      return `Average life expectancy for your demographic on ${this.planet}: ${Math.round(this.expectancy / 11.86)}. Your age in ${this.planet} years: ${Math.round(this.age / 11.86)}. Years lived beyond average life expectancy: ${Math.round((this.age - this.expectancy) / 11.86)}`;
     } else {
       return `Average life expectancy for your demographic on ${this.planet}: ${Math.round(this.expectancy / 11.86)}. Your age in ${this.planet} years: ${Math.round(this.age / 11.86)}. ${this.planet} years left to live: ${Math.round((this.expectancy - this.age) / 11.86)}`;
     }
