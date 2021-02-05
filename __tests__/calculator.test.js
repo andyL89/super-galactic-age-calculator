8d4cc6ffc2e93b8cfbfe77d3f59f4afceb08e51a
Andy Lawson <alawson89@gmail.com>
2021-02-05 08:46:09 -0800

    add good fail on 'describe calculator' test

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -1,1 +1,9 @@
+import { TestScheduler } from "jest";
 import Calculator from "../src/js/calculator.js";
+
+describe("Calculator", () => {
+  test("Should return a calculator with planet created.", () => {
+    const newCalculator = new Calculator("planet");
+    expect(newCalculator.planet).toEqual("planet");
+  });
+});
--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,5 +1,5 @@
-export class Calculator {
-  constructor (days) {
-
+export default class Calculator {
+  constructor (planet) {
+    
   }
 }
